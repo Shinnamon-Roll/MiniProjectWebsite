b4642bf834330fa4ec69aada0c1622d98af03915
Shinnamon-Roll <chimonkung0@gmail.com>
2025-05-22 17:17:16 +0700

    what

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const mainMenu = document.getElementById("mainMenu");
+const miniHeight = document.querySelector(".miniMenu").offsetHeight;
+
+window.addEventListener("scroll", () => {
+    if (window.scrollTo > miniHeight) {
+        mainMenu.classList.add()
+    }
+})
